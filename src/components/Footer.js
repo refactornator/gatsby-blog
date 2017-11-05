f9a40087e4081e03e80e3cd37e5f39a1d51e0209
William Lindner <wlindner522@gmail.com>
2017-11-04 22:39:32 -0700

    Footer cleanup on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { rhythm } from '../utils/typography'
 
 const Footer = styled.footer`
   color: #666;
-  max-width: 860px;
+  width: 100%;
   height: 60px;
   display: flex;
   margin: 0 auto;
@@ -16,10 +16,29 @@ const Footer = styled.footer`
   text-transform: uppercase;
   justify-content: space-between;
   border-top: 1px solid ${colors.store};
+
+  @media all and (max-width: 860px) {
+    display: block;
+    padding: 0 20px;
+    margin-bottom: 10px;
+
+    ${Block} {
+      font-size: 12px;
+      text-align: left;
+    }
+  }
+
+  @media all and (min-width: 860px) {
+    width: 860px;
+
+    ${Block} {
+      line-height: normal;
+    }
+  }
 `
 
 const Block = styled.div`
-  color: ${colors.store};
+  color: ${colors.snuggles};
   letter-spacing: 0.2em;
   line-height: 32px;
 `
