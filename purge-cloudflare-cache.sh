fdefb9558faea81e100ee51482763bf1bc67c68e
William Lindner <wlindner522@gmail.com>
2019-07-20 20:59:02 -0700

    Purge cloudflare cache post deploy

---
+++ purge-cloudflare-cache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -v -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache" \
+    -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+    -H "X-Auth-Key: ${CLOUDFLARE_KEY}" \
+    -H "Content-Type: application/json" \
+    --data '{"purge_everything":true}'
