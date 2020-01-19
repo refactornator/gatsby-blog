8c6b8b04f2b588bbf309df6fd6fecc85a5c7455e
William Lindner <wlindner522@gmail.com>
2020-01-19 15:40:33 -0800

    Add initial faunadb integration

---
+++ scripts/bootstrap-fauna-database.js
@@ -0,0 +1,96 @@
+/* bootstrap database in your FaunaDB account */
+const readline = require('readline')
+const faunadb = require('faunadb')
+const chalk = require('chalk')
+const insideNetlify = insideNetlifyBuildContext()
+const q = faunadb.query
+
+console.log(chalk.cyan('Creating your FaunaDB Database...\n'))
+
+// 1. Check for required enviroment variables
+if (!process.env.FAUNADB_SECRET) {
+  console.log(
+    chalk.yellow('Required FAUNADB_SECRET enviroment variable not found.')
+  )
+  if (insideNetlify) {
+    console.log(
+      `Visit https://app.netlify.com/sites/modest-shockley-6df843/settings/deploys`
+    )
+    console.log(
+      'and set a `FAUNADB_SECRET` value in the "Build environment variables" section'
+    )
+    process.exit(1)
+  }
+  // Local machine warning
+  if (!insideNetlify) {
+    console.log()
+    console.log(
+      'You can create fauna DB keys here: https://dashboard.fauna.com/db/keys'
+    )
+    console.log()
+    ask(chalk.bold('Enter your faunaDB server key'), (err, answer) => {
+      if (!answer) {
+        console.log('Please supply a faunaDB server key')
+        process.exit(1)
+      }
+      createFaunaDB(answer).then(() => {
+        console.log('Database created')
+      })
+    })
+  }
+}
+
+// Has var. Do the thing
+if (process.env.FAUNADB_SECRET) {
+  createFaunaDB(process.env.FAUNADB_SECRET).then(() => {
+    console.log('Database created')
+  })
+}
+
+function createFaunaDB(key) {
+  console.log('Create the database!')
+  const client = new faunadb.Client({
+    secret: key,
+  })
+
+  return client
+    .query(q.CreateCollection({ name: 'likes' }))
+    .then(() => {
+      return client.query(
+        q.CreateIndex({
+          name: 'all_likes',
+          source: q.Collection('likes'),
+        })
+      )
+    })
+    .catch(e => {
+      if (
+        e.requestResult.statusCode === 400 &&
+        e.message === 'instance already exists'
+      ) {
+        console.log('DB already exists')
+      } else {
+        console.log('something went wrong: ', e)
+      }
+    })
+}
+
+// Test if inside netlify build context
+function insideNetlifyBuildContext() {
+  if (process.env.DEPLOY_PRIME_URL) {
+    return true
+  }
+  return false
+}
+
+// Readline util
+function ask(question, callback) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  rl.question(question + '\n', function(answer) {
+    rl.close()
+    callback(null, answer)
+  })
+}
--- src/lambda/like.js
+++ src/lambda/like.js
@@ -1,3 +1,11 @@
+import url from 'url'
+import faunadb from 'faunadb'
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET,
+})
+
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 export function handler(event, context, callback) {
   console.log('event: ', event)
@@ -5,10 +13,28 @@ export function handler(event, context, callback) {
 
   switch (event.httpMethod) {
     case 'POST':
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({}),
-      })
+      const { referer, host } = event.headers
+      var refererUrl = url.parse(referer)
+      client
+        .query(
+          q.Create(q.Collection('likes'), {
+            data: { host, path: refererUrl.pathname },
+          })
+        )
+        .then(response => {
+          console.log('success', response)
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(response),
+          })
+        })
+        .catch(error => {
+          console.log('error', error)
+          callback(null, {
+            statusCode: 400,
+            body: JSON.stringify(error),
+          })
+        })
       break
     default:
       callback(null, {
