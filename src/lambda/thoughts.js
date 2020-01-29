3b439ec59c7b89f931775d034ae13e8372c690aa
William Lindner <wlindner522@gmail.com>
2020-01-29 14:58:21 -0800

    Rewrite the thoughts lambda to use async/await

--- src/lambda/thoughts.js
+++ src/lambda/thoughts.js
@@ -1,4 +1,3 @@
-import url from 'url'
 import faunadb from 'faunadb'
 
 const q = faunadb.query
@@ -7,67 +6,64 @@ const adminClient = new faunadb.Client({
 })
 
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
-export function handler (event, context, callback) {
+export async function handler (event, context) {
   console.log(`event.body: ${event.body}`)
 
-  adminClient
+  const { secret } = await adminClient
     .query(
       q.CreateKey({
         database: q.Database('blog'),
         role: 'server',
       })
     )
-    .then(({ secret }) => {
-      const dbClient = new faunadb.Client({
-        secret,
-      })
 
-      switch (event.httpMethod) {
-        case 'GET':
-          dbClient
-            .query(q.Map(q.Paginate(q.Match(q.Index('all_thoughts'))), q.Lambda('x', q.Get(q.Var('x')))))
-            .then(response => {
-              console.log('success', response)
-              callback(null, {
-                statusCode: 200,
-                body: JSON.stringify(response),
-              })
-            })
-            .catch(error => {
-              console.log('error', error)
-              callback(null, {
-                statusCode: 400,
-                body: JSON.stringify(error),
-              })
-            })
-          break
-        case 'POST':
-          dbClient
-            .query(
-              q.Create(q.Collection('thoughts'), {
-                data: { text: event.body },
-              })
-            )
-            .then(response => {
-              console.log('success', response)
-              callback(null, {
-                statusCode: 200,
-                body: JSON.stringify(response),
-              })
-            })
-            .catch(error => {
-              console.log('error', error)
-              callback(null, {
-                statusCode: 400,
-                body: JSON.stringify(error),
-              })
-            })
-          break
-        default:
-          callback(null, {
-            statusCode: 404,
-            body: JSON.stringify({}),
-          })
+  const dbClient = new faunadb.Client({
+    secret,
+  })
+
+  switch (event.httpMethod) {
+    case 'GET':
+      try {
+        const response = await dbClient
+          .query(q.Map(q.Paginate(q.Match(q.Index('all_thoughts'))), q.Lambda('x', q.Get(q.Var('x')))))
+
+        console.log('success', response)
+        return {
+          statusCode: 200,
+          body: JSON.stringify(response),
+        }
+      } catch (error) {
+        console.log('error', error)
+        return {
+          statusCode: 400,
+          body: JSON.stringify(error),
+        }
       }
-    })
+    case 'POST':
+      try {
+        const response = await dbClient
+          .query(
+            q.Create(q.Collection('thoughts'), {
+              data: { text: event.body },
+            })
+          )
+
+        console.log('success', response)
+        return {
+          statusCode: 200,
+          body: JSON.stringify(response),
+        }
+      } catch (error) {
+        console.log('error', error)
+        return {
+          statusCode: 400,
+          body: JSON.stringify(error),
+        }
+      }
+    default:
+      return {
+        statusCode: 404,
+        body: JSON.stringify({}),
+      }
+  }
 }
