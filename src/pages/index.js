e88f708c877a18ed116f445ca1c4fd18fc0758e2
William Lindner <wlindner522@gmail.com>
2020-02-09 21:15:13 -0800

    Fix stuff on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import webm from '../video/intro.webm'
 import mp4 from '../video/intro.mp4'
 
 const Summary = styled.div`
+  display: flex;
   margin-bottom: 40px;
 
   @media all and (max-width: 860px) {
@@ -18,9 +19,12 @@ const Summary = styled.div`
   }
 `
 
+const Content = styled.div`
+  max-width: 900px;
+`
+
 const IntroVideo = styled.video`
-  width: 200px;
-  float: right;
+  width: 270px;
 `
 
 export default ({ data }) => {
@@ -38,38 +42,37 @@ export default ({ data }) => {
   return (
     <Layout title={title}>
       <Summary>
+        <Content>
+          <h1>Welcome to William's website.</h1>
+          <h2>A place to share my thoughts.</h2>
+          <p>
+            <a href="#" onClick={handleClickOpen}>
+              Send me a message
+            </a>{' '}
+            if you want to contact me about something.
+          </p>
+          <p>
+            I sometimes write code on{' '}
+            <a target="_blank" href="https://github.com/wlindner">
+              https://github.com/wlindner
+            </a>
+            .
+          </p>
+          <p>
+            I never check{' '}
+            <a target="_blank" href="https://www.linkedin.com/in/wlindner">
+              https://www.linkedin.com/in/wlindner
+            </a>
+            .
+          </p>
+          <p>
+            Check out my <Link to="/essays">essays</Link>.
+          </p>
+        </Content>
         <IntroVideo autoPlay loop controls alt="William Lindner">
           <source src={webm} type="video/webm" />
           <source src={mp4} type="video/mp4" />
         </IntroVideo>
-        <h1>Welcome to William's website.</h1>
-        <h2>A place to share my thoughts.</h2>
-        <p>
-          <a href="#" onClick={handleClickOpen}>
-            Send me a message
-          </a>{' '}
-          if you want to contact me about something.
-        </p>
-
-        <p>
-          I sometimes write code on{' '}
-          <a target="_blank" href="https://github.com/wlindner">
-            https://github.com/wlindner
-          </a>
-          .
-        </p>
-
-        <p>
-          I never check{' '}
-          <a target="_blank" href="https://www.linkedin.com/in/wlindner">
-            https://www.linkedin.com/in/wlindner
-          </a>
-          .
-        </p>
-
-        <p>
-          Check out my <Link to="/essays">essays</Link>.
-        </p>
       </Summary>
       <FeedbackDialog onClose={handleClose} open={dialogOpen} />
     </Layout>
