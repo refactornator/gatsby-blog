0462994afc2bf10efb6efd3a40ca8d46bbf3086a
William Lindner <wlindner522@gmail.com>
2020-03-24 19:49:51 -0700

    Upgrade blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const proxy = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
@@ -9,10 +9,10 @@ module.exports = {
   },
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  developMiddleware: app => {
+  developMiddleware: (app) => {
     app.use(
       '/.netlify/functions/',
-      proxy({
+      createProxyMiddleware({
         target: 'http://localhost:9000',
         pathRewrite: {
           '/.netlify/functions/': '',
